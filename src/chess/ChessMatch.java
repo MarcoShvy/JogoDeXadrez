3318a5bae2646a8fb7343d6a3a097454d6364c91
Marco <mass7813@gmail.com>
2023-08-05 14:30:16 -0300

    Position

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -24,9 +25,11 @@ public class ChessMatch {
         }
         return mat;
     }
-
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column,row).toPosition());
+    }
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,4));
-        board.placePiece(new King(board, Color.WHITE), new Position(2,4));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
     }
 }
