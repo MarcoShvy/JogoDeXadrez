edcfd1dc2ea38f35ffe2e59adc84fcb5caa53e4c
Marco <mass7813@gmail.com>
2023-08-05 16:50:32 -0300

    King possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -43,13 +43,14 @@ public class UI {
     }
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8-i)+" ");
+            System.out.print((8-i)+"   ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
-        System.out.println("  A B C D E F G H");
+        System.out.println();
+        System.out.println("    A B C D E F G H");
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
