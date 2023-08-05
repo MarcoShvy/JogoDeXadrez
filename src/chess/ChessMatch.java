a60cdbbe4d77f53fb5d05eb44359be0232e95d4e
Marco <mass7813@gmail.com>
2023-08-05 16:21:06 -0300

    Piece moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -40,6 +40,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -47,8 +48,15 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("there is no possible moves! ");
+        }
     }
 
+    private void validateTargetPosition(Position source, Position target) {
+        if(!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");        }
+    }
     private Piece makeMove(Position source, Position target) {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
