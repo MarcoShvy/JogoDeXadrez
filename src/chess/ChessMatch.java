16b9cc6357983b0738ecbe7d4f661b86dac02e90
Marco <mass7813@gmail.com>
2023-08-06 15:06:51 -0300

    New pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,10 +4,7 @@ import boardgame.Board;
 import boardgame.BoardExecption;
 import boardgame.Piece;
 import boardgame.Position;
-import chess.pieces.Bishop;
-import chess.pieces.King;
-import chess.pieces.Pawn;
-import chess.pieces.Rook;
+import chess.pieces.*;
 import chess.ChessPiece;
 
 import java.util.ArrayList;
@@ -82,7 +79,11 @@ public class ChessMatch {
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('e', 3, new Bishop(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('b', 1, new Horse(board, Color.WHITE));
+        placeNewPiece('g', 1, new Horse(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
@@ -95,6 +96,11 @@ public class ChessMatch {
         placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('b', 8, new Horse(board, Color.BLACK));
+        placeNewPiece('g', 8, new Horse(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
     }
 
     public ChessPiece perfomChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
--- src/chess/pieces/Horse.java
+++ src/chess/pieces/Horse.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -10,12 +11,60 @@ public class Horse extends ChessPiece {
         super(board, color);
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p =(ChessPiece)getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+
+        // 2 frente 1 direita
+        p.setValues(position.getRow()-2, position.getColumn() +1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 2 frente 1 esquerda
+        p.setValues(position.getRow()-2, position.getColumn() -1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 1 frente 2 direita
+        p.setValues(position.getRow()-1, position.getColumn() +2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 1 frente 2 esquerda
+        p.setValues(position.getRow() -1, position.getColumn() -2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 2 atras 1 direita
+        p.setValues(position.getRow()+2, position.getColumn() +1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 2 atras 1 esquerda
+        p.setValues(position.getRow()+2, position.getColumn() -1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 1 atras 2 direita
+        p.setValues(position.getRow()+1, position.getColumn() +2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+        // 1 atras 2 esquerda
+        p.setValues(position.getRow() +1, position.getColumn() -2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
     }
 
-        
+
     @Override
     public String toString() {
         return "H";
